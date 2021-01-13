bd91da4c4bdd7f3c619b359c9bcd79743f56c317
Andrea Ramos <andrearamosjn@gmail.com>
2021-01-14 00:49:12 +0100

    deploy

--- code.js
+++ code.js
@@ -1,26 +1,14 @@
-function actualizarLinkSeleccionado() {
-    if (true) return;
-    $('a.scrollLink').each(function(idx, el) {
-      const nLink = $(el);
-      if (nLink.attr('href') && nLink.attr('href').match(/(#.*)/)[1] === window.location.hash) {
-        return nLink.addClass('selected')
-      }
-      nLink.removeClass('selected')
-    })
-}
 
 $(document).ready(function(){
   $( "a" ).click(function() {
     const href = $(this).attr('href');
     if (href.charAt(0) !== '#') return;
-    $("html, body").animate({ scrollTop: $(href).offset().top }, 100);
+    // $("html, body").animate({ scrollTop: $(href).offset().top }, 100);
   });
-  window.onhashchange = actualizarLinkSeleccionado;
-  actualizarLinkSeleccionado();
 
   var controller = new ScrollMagic.Controller();
-  new ScrollMagic.Scene({triggerElement: "#netflix"})
-    .setClassToggle("#work-link", "selected-netflix") // add class toggle
+  new ScrollMagic.Scene({triggerElement: "#hiddenGems"})
+    .setClassToggle("#work-link", "selected-hiddenGems") // add class toggle
     .addTo(controller);
   new ScrollMagic.Scene({triggerElement: "#respira"})
     .setClassToggle("#work-link", "selected-respira") // add class toggle
@@ -28,4 +16,21 @@ $(document).ready(function(){
   new ScrollMagic.Scene({triggerElement: "#womenInTech"})
     .setClassToggle("#work-link", "selected-womenInTech") // add class toggle
     .addTo(controller);
+});
+
+
+//Navegación de los proyectos con scroll
+$(window).scroll(function() {
+  var position = $(this).scrollTop();
+    $('.seccion-proyecto').each(function() {
+      var target = $(this).offset().top - 250;
+      var id = $(this).attr('id');
+
+      if (position >= target) {
+        $('#navigation > ul > li').removeClass('active');
+        $('#navigation > ul > li').removeClass('border-nav');
+        $('#navigation > ul > li[href="#' + id + '"]').addClass('active');
+        $('#navigation > ul > li[href="#' + id + '"]').addClass('border-nav');
+      }
+    });
 });
